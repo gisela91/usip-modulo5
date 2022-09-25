4ab91e69dae0f0486965ad2b0a92dbfdf9f4e389
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-24 22:46:19 -0400

    refresh

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria
+from .models import Producto
+from .models import Orden
+from .models import OrdenProducto
+
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Orden)
+admin.site.register(OrdenProducto)
 
 # Register your models here.
--- inventario/models.py
+++ inventario/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 class Categoria(models.Model):
 	nombre = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.nombre
 
 class ProductUnits(models.TextChoices):
 	UNITS = 'u', 'Unidades'
@@ -21,3 +25,27 @@ class Producto(models.Model):
 	create = models.DateTimeField(auto_now_add=True)
 	update = models.DateTimeField(auto_now=True)
 
+class EstadoOrden(models.TextChoices):
+	NOPAGADO = 'nopagado', 'No pagado'
+	PAGADO = 'pagado', 'Pagado'
+
+class Orden(models.Model):
+	total = models.IntegerField(default=0)
+	fecha = models.DateField()
+	vendedor =  models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE,
+		related_name="inventario_orden_vendedor"
+		)
+	estado = models.CharField(
+		max_length=10,
+		choices=EstadoOrden.choices,
+		default=EstadoOrden.NOPAGADO
+		)
+
+class OrdenProducto(models.Model):
+	orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+	producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+	cantidad = models.IntegerField(default=0)
+	precio = models.DecimalField(decimal_places=2,max_digits=10)
+	
